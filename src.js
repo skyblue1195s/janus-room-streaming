983c5fe8da733cb2a07129314940b6289825d18f
Sky Blue <>
2021-05-20 17:27:46 +0700

    hange location to window.location

--- src.js
+++ src.js
@@ -19,7 +19,7 @@ window.remotestreams = config.remotestreams;
 function getQueryStringValue(name) {
   name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
   var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
-  var results = regex.exec(location.search);
+  var results = regex.exec(window.location.search);
   return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
